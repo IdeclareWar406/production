c5a91ae99c4b3a66816a73a71e1f631abe86762d
IdeclareWar406 <120147127+IdeclareWar406@users.noreply.github.com>
2023-09-10 00:14:09 -0400

    more changes

--- moveChristianChurch/moveChristianChurch/src/components/Mens.jsx
+++ moveChristianChurch/moveChristianChurch/src/components/Mens.jsx
@@ -8,7 +8,7 @@ export default function Mens(){
     return(
         <div className="mensMinistry">
             <h2 className="menMove" >Men on the Move</h2>
-           <img className="mensImg" src="public/images/MensMinistry.jpeg" ></img>
+           <img className="mensImg" src="images/MensMinistry.jpeg" ></img>
            <h2>Men, join us as we gather together for fellowship, to lead, serve our families, congregation, and community</h2>
            <h2>Contact Mike Dutton, Ivan Pate or Andy Brock</h2>
         </div>
