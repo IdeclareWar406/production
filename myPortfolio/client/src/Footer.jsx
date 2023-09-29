9506e4dc5b6323781d91b20cbf51bf87790477c8
IdeclareWar406 <120147127+IdeclareWar406@users.noreply.github.com>
2023-09-28 23:12:36 -0400

    more updates

--- myPortfolio/client/src/Footer.jsx
+++ myPortfolio/client/src/Footer.jsx
@@ -15,16 +15,16 @@ const path= window.location
       
         </div>
     )}
-    if(path.pathname === '/'){
-        return(
-            <div style={{position: 'relative', top: '150%'}} className="footer">
+    // if(path.pathname === '/'){
+    //     return(
+    //         <div style={{position: 'relative', top: '150%'}} className="footer">
     
-            <h2>Thank you for visiting</h2>
-            <Link className="contact" to='/contact'>Click here for contact information</Link>
+    //         <h2>Thank you for visiting</h2>
+    //         <Link className="contact" to='/contact'>Click here for contact information</Link>
           
-            </div>
-        )
-    }
+    //         </div>
+    //     )
+    // }
     else 
     return(
         <div style={{position: 'relative', top: '275%'}} className="footer">
