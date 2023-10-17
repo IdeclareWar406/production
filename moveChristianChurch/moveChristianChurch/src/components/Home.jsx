c4a540046eaa95356c2d2a377f097dfddb5cb33d
IdeclareWar406 <120147127+IdeclareWar406@users.noreply.github.com>
2023-10-17 18:31:25 -0400

    css updates

--- moveChristianChurch/moveChristianChurch/src/components/Home.jsx
+++ moveChristianChurch/moveChristianChurch/src/components/Home.jsx
@@ -51,7 +51,7 @@ document.body.style.backgroundColor = "black"
 
         <div className="homeFooter">
             <h2 className="ourAddress" style={{color: "white"}} > Our location is 1501 Whispering Pines Rd, Albany, GA</h2><a  className="directions" href="https://www.google.com/maps/place/Move+Christian+Church/@31.605381,-84.185001,15z/data=!4m6!3m5!1s0x88f27bf5efc37f83:0x8436521efbd5f2d!8m2!3d31.6054188!4d-84.1845076!16s%2Fg%2F1tp0fhnr?entry=ttu" target="_blank">Directions</a> 
-            <Link to='/contactus' className="ourEmail" style={{color:"white"}} >Contact Us</Link>
+            <Link  to='/contactus' className="ourEmail" style={{color:"white"}} >Contact Us</Link>
         </div>
         </div>)
 }
