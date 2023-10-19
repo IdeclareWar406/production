5f53eda6232bc214011143a860f6261fa511d202
IdeclareWar406 <120147127+IdeclareWar406@users.noreply.github.com>
2023-10-19 17:28:27 -0400

    updates

--- moveChristianChurch/moveChristianChurch/src/components/OurLeadership.jsx
+++ moveChristianChurch/moveChristianChurch/src/components/OurLeadership.jsx
@@ -14,6 +14,7 @@ export default function OurLeadership(props){
             <h3>Freddie has previously served our church, in its earlier years, as the lead minister.  He now serves as pastor to our seniors, and hosts an in-depth bible study on Sunday mornings.  Freddy was married to his late wife, Wendy for 50+ years.  He is a real blessing to Move Christian Church. </h3>
             <h2>Youth Pastor - Dalton Smith</h2>
             <h3>Dalton has been working with our youth since August of 2019. He has one daughter, and loves the Lord!</h3>
+            <h2>Kids on the Move Director - Malynn Petty</h2>
            <Elder elder={props.elders} />
         </div>
     )
